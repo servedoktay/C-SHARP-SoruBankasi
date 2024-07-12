7261e0b967b8fce603a8707ddbda16687e33e5bd
servedoktay <49237269+servedoktay@users.noreply.github.com>
2024-07-12 07:11:23 +0300

    Add files via upload

---
+++ sorubankbolum5/cozumluSoruBankSoru15/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cozumluSoruBankSoru16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int sayi ,toplam, ort;
+
+            //toplam = 0;
+
+            //  for (int i = 0; i < 16; i++)
+            //  {
+            //      Console.WriteLine($"{i + 1}.Sayi Girisi");
+            //      sayi = Convert.ToInt32(Console.ReadLine());
+            //      toplam = toplam + sayi;
+            //      sayi = 0;
+            //  }
+            //  ort = toplam / 16;
+            //  Console.WriteLine($"16 sayinin toplami={toplam}");
+            //  Console.WriteLine($"Ort={toplam/16}");
+
+            int toplam, ort;
+            int[] sayilar = new int[16];
+            toplam = 0;
+
+            for (int i = 0; i < 16; i++)
+            {
+                Console.WriteLine($"{i + 1}.Sayi Girisi");
+                sayilar[i] = Convert.ToInt32(Console.ReadLine());
+                toplam = toplam + sayilar[i];
+               
+            }
+            ort = toplam / 16;
+            Console.WriteLine($"16 sayinin toplami={toplam}");
+            Console.WriteLine($"Ort={toplam / sayilar.Length}");
+
+
+            Console.ReadKey();
+
+        }   
+    }
+}
---
+++ sorubankbolum5/cozumluSoruBankSoru17/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cozumluSoruBankSoru17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int s1, s2, enb, enk;
+            Console.WriteLine("S1:");
+            s1=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("S2:");
+            s2=Convert.ToInt32(Console.ReadLine());
+
+            enb = s1; enk = s1;
+            if (s2 > enb) enb = s2;
+            else if (s2 < enk) enk = s2;
+            else if(s2 == enk) Console.WriteLine("Esit Degerler");
+            Console.WriteLine("Aralarındaki Cift Sayilar");
+            for (int i = enk; i < enb; i++)
+            {
+                if (i % 2 == 0) Console.WriteLine(i);
+                else continue;
+            }
+            Console.ReadKey();
+        }
+    }
+}
